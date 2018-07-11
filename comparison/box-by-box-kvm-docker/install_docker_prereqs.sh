7e5bd9f6854760c3c101656e5e7c051beb66a0ae
Michael S. Pedersen <michaelx.pedersen@intel.com>
2018-07-11 11:20:24 -0700

    Added hugepages to docker prereqs

--- comparison/box-by-box-kvm-docker/install_docker_prereqs.sh
+++ comparison/box-by-box-kvm-docker/install_docker_prereqs.sh
@@ -19,3 +19,14 @@ sudo add-apt-repository -y  \
 sudo apt-get update
 
 sudo apt-get install -y docker-ce
+
+# Prepare Hugepages
+for i in $(ls /sys/devices/system/node/ | grep node); do
+  if [ "$(sudo cat /sys/devices/system/node/${i}/hugepages/hugepages-2048kB/nr_hugepages)" == "0" ]; then
+    sudo echo 2048 > /sys/devices/system/node/${i}/hugepages/hugepages-2048kB/nr_hugepages
+  fi
+done
+
+if [ ! -d "/dev/hugepages" ]; then
+  sudo mount -t hugetlbfs -o pagesize=2M none /dev/hugepages
+fi 
