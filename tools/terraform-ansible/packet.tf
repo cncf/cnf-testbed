cdb2a232992d409a42403264e27592383c9d2699
denverwilliams <denver@debian.nz>
2018-10-29 09:12:17 +0000

    Add Terraform and reboot support for ansible deploys #91

---
+++ tools/terraform-ansible/packet.tf
@@ -0,0 +1,19 @@
+resource "packet_device" "cnfs" {
+  count            = "${ var.packet_node_count }"
+  hostname         = "${ var.name }${ count.index + 1 }"
+  facility         = "${ var.packet_facility }"
+  project_id       = "${ var.packet_project_id }"
+  plan             = "${ var.packet_master_device_plan }"
+  billing_cycle    = "${ var.packet_billing_cycle }"
+  operating_system = "${ var.packet_operating_system }"
+ 
+  connection {}
+  
+  provisioner "ansible" {
+    plays {
+      playbook ={
+        file_path = "/root/cnfs/comparison/cnf_edge_throughput/packet_generator/ansible/main.yml"
+      }
+    }
+  }
+}
---
+++ tools/terraform-ansible/variables.tf
@@ -0,0 +1,13 @@
+provider "packet" {
+  auth_token = "${ var.packet_api_key }"
+}
+variable "name" { default = "pktgen" }
+variable "packet_node_count" { default = "1" }
+variable "packet_project_id" {}
+variable "packet_api_key" {}
+variable "packet_facility" { default = "sjc1" }
+variable "packet_billing_cycle" { default = "hourly" }
+
+# VM Image and size
+variable "packet_master_device_plan" { default = "c1.xlarge.x86" }
+variable "packet_operating_system" { default = "ubuntu_16_04" }
