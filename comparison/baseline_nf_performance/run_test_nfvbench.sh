8256f0339bf7868ea0afe132bf337b52ab6315e4
Peter Mikus <pmikus@MacBook-Pro.local>
2018-11-01 15:46:30 +0100

    [CSIT] Move to nfvbench2.0.1 for Chained NFs Test Cases [#92]
    Currently there is configuration for nfvbench1.5.2 which is not
    capable of sending traffic streams with more than 1 VLAN per
    direction. This commit is supposed to move to nfvbench2.0.1 which
    is reportedly able to create stream for multiple chains [0].
    
    [0] https://raw.githubusercontent.com/opnfv/nfvbench/master/docs/release/release-notes/release-notes.rst

---
+++ comparison/baseline_nf_performance/run_test_nfvbench.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+function die () {
+    # Print the message to standard error end exit with error code specified
+    # by the second argument.
+    #
+    # Hardcoded values:
+    # - The default error message.
+    # Arguments:
+    # - ${1} - The whole error message, be sure to quote. Optional
+    # - ${2} - the code to exit with, default: 1.
+
+    set -x
+    set +eu
+    warn "${1:-Unspecified run-time error occurred!}"
+    exit "${2:-1}"
+}
+
+function warn () {
+    # Print the message to standard error.
+    #
+    # Arguments:
+    # - ${@} - The text of the message.
+
+    echo "$@" >&2
+}
+
+function run_bench () {
+    # Runs nvfbench and output results into directory
+    #
+    # Variables read:
+    # - BASH_FUNCTION_DIR - Path to script directory.
+    # - RATES - Path to script directory.
+    # - ITERATIONS - Number of bench iterations.
+    # - DURATION - Duration of bench iterations.
+    # - PREFIX - Prefix for results.
+
+    pushd "${BASH_FUNCTION_DIR}" || die "Change dir failed!"
+    out_dir="results/${PREFIX}/${NODENESS}"
+    if [ ! -d "${out_dir}" ]; then
+        warn "Creating directory ${out_dir}"
+        mkdir -p "${out_dir}" || die "Create output dir failed!"
+    fi
+    for rate in "${RATES[@]}"; do
+        for iter in $(seq 1 "${ITERATIONS}"); do
+            warn "Running test: ${PREFIX}, Rate: ${rate}, Iteration ${iter}"
+            dcr_image="nfvbench"
+            dcr_param="--interactive "
+            dcr_param+="--tty "
+            nfv_param="nfvbench -c /tmp/nfvbench/nfvbench_config.cfg "
+            nfv_param+="--rate ${rate} "
+            nfv_param+="--flow-count 1000 "
+            nfv_param+="--duration ${DURATION} "
+            results="${out_dir}/${PREFIX}-${rate}-${iter}.log"
+            params=(${dcr_param} ${dcr_image} ${nfv_param})
+            sudo docker exec "${params[@]}" 2>&1 | tee -a "${results}"
+        done
+    done
+    popd || die "Change dir failed!"
+}
+
+BASH_FUNCTION_DIR="$(dirname "$(readlink -e "${BASH_SOURCE[0]}")")" || {
+    die "Some error during localizing this source directory."
+}
+PREFIX="vsc"
+RATES=( ndr )
+ITERATIONS=1
+DURATION=30
+NODENESS=${1:-1}
+
+run_bench || die
