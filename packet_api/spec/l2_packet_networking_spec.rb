d1bb0b4ac8a8ed3d3b0f484d42f17829658183d5
wolfpack <wolfpack@vulk.co>
2018-11-03 09:36:38 -0400

    #90 --delete-vlan and --assign-vlan now pass tests.  --facility now needed for assign, unassign, and delete vlan

--- packet_api/spec/l2_packet_networking_spec.rb
+++ packet_api/spec/l2_packet_networking_spec.rb
@@ -1,35 +1,64 @@
 require 'spec_helper'
 
  # *** Assign your packet api token in the .env file and then source it before running rspec ***
- RSpec.describe 'Packet Test Suite', type: :aruba  do
-   context "Packet test" do
-     it 'tests disbond-interface' do
-       cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --disbond-interface eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
-       run(cmd)
-       sleep(10)
-       stop_all_commands
+RSpec.describe 'Packet Test Suite', type: :aruba  do
+  context "Packet test" do
+    it 'tests disbond-interface' do
+      cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --disbond-interface eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
+      run(cmd)
+      sleep(10)
+      stop_all_commands
 
-       expect(last_command_started.output).to eq("success\n")
-     end
-     it 'tests create-vlan' do
-       cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNCF CNFs' --packet-url='api.packet.net'"
-       run(cmd)
-       sleep(10)
-       stop_all_commands
+      expect(last_command_started.output).to eq("success\n")
+    end
+    it 'tests create-vlan' do
+      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNCF CNFs' --packet-url='api.packet.net' --facility='ewr1' "
+      run(cmd)
+      sleep(10)
+      stop_all_commands
 
-       expect(last_command_started.output.to_i).to be_a_kind_of(Numeric)
-     end
-     it 'tests assign-vlan.rb' do
-       cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --assign-vlan watsonvlan1 --assign-vlan-port eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
-       run(cmd)
-       sleep(10)
-       stop_all_commands
+      expect(last_command_started.output.to_i).to be_a_kind_of(Numeric)
+    end
+    it 'tests assign-vlan.rb' do
+      cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --assign-vlan watsonvlan1 --assign-vlan-port eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
+      run(cmd)
+      sleep(10)
+      stop_all_commands
 
-       # puts "last command started output: #{last_command_started.output}"
-       # puts "last command started stderr: #{last_command_started.stderr}"
-       # puts "last command started stdout: #{last_command_started.stdout}"
-       expect(last_command_started.output).to eq("success\n")
-     end
-   end
- end
+      # puts "last command started output: #{last_command_started.output}"
+      # puts "last command started stderr: #{last_command_started.stderr}"
+      # puts "last command started stdout: #{last_command_started.stdout}"
+      expect(last_command_started.output).to eq("success\n")
+    end
+
+    it 'tests unassign-vlan.rb' do
+      cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --assign-vlan watsonvlan1 --assign-vlan-port eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
+      run(cmd)
+      sleep(10)
+      stop_all_commands
+
+      cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --unassign-vlan watsonvlan1 --unassign-vlan-port eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
+      run(cmd)
+      sleep(10)
+      stop_all_commands
+
+      # puts "last command started output: #{last_command_started.output}"
+      # puts "last command started stderr: #{last_command_started.stderr}"
+      # puts "last command started stdout: #{last_command_started.stdout}"
+      expect(last_command_started.output).to eq("success\n")
+    end
+    it 'tests delete-vlan' do
+      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNCF CNFs' --packet-url='api.packet.net' --facility='ewr1' "
+      run(cmd)
+      sleep(10)
+      stop_all_commands
+
+      cmd = "ruby ../../l2_packet_networking.rb --delete-vlan watsonvlan1 --project-name='CNCF CNFs' --packet-url='api.packet.net' ty='ewr1' "
+      run(cmd)
+      sleep(10)
+      stop_all_commands
+      expect(last_command_started.output).to eq("success\n")
+    end
+  end
+end
 
