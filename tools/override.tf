f2efe1882ffb454aa036faa8e532dfdf2d60c55a
denverwilliams <denver@debian.nz>
2018-11-03 20:16:20 -0400

    Add env config #99

--- tools/override.tf
+++ tools/override.tf
@@ -6,12 +6,12 @@ module "worker_templates" {
     source = "../worker_templates-v1.10.0-ubuntu"
 }
 
-resource "packet_device" "masters" {
-  hardware_reservation_id = "your-reservation_id"
-}
+#resource "packet_device" "masters" {
+#  hardware_reservation_id = "your-reservation_id"
+#}
 
-resource "packet_device" "workers" {
-  hardware_reservation_id = "your-reservation_id"
-}
+#resource "packet_device" "workers" {
+#  hardware_reservation_id = "your-reservation_id"
+#}
 
 
