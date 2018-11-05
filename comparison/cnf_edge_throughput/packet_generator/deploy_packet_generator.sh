39055995bfe3021b796ee2a7346530b6d1adfe64
wolfpack <wolfpack@vulk.co>
2018-11-05 18:21:41 -0500

    initial renaming to support both mellanox and intel nics in provisioning [#91]

--- comparison/cnf_edge_throughput/packet_generator/deploy_packet_generator.sh
+++ comparison/cnf_edge_throughput/packet_generator/deploy_packet_generator.sh
@@ -3,13 +3,23 @@ dir=$(pwd)
 parentdir="$(dirname "$dir")"
 parentdir2="$(dirname "$parentdir")"
 parentdir3="$(dirname "$parentdir2")"
-echo "DIR $parentdir3"
+
+
+if [ "$1" = "dual_mellanox" ] ; then
+  PLAYBOOK_NAME="packet_generator_dual_mellanox.yml"
+elif [ "$1" = "quad_intel" ] ; then
+  PLAYBOOK_NAME="packet_generator_dual_mellanox.yml"
+else
+  echo "Usage: $0 <dual_mellanox|quad_intel>"
+  exit 1
+fi
 
 docker build -t cnfdeploytools:latest .
+
 docker run -v $(pwd)/ansible:/ansible -v ~/.ssh/id_rsa:/root/.ssh/id_rsa -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub -v "$parentdir3"/tools/terraform-ansible/:/terraform \
   -e TF_VAR_packet_project_id=${PACKET_PROJECT_ID} \
   -e TF_VAR_packet_api_key=${PACKET_AUTH_TOKEN} \
   -e TF_VAR_packet_facility=${PACKET_FACILITY} \
   -e TF_VAR_packet_master_device_plan=${PACKET_MASTER_DEVICE_PLAN} \
   -e TF_VAR_packet_operating_system=${PACKET_OPERATING_SYSTEM} \
-  -e TF_VAR_playbook=/ansible/main.yml -ti cnfdeploytools:latest apply -auto-approve
+  -e TF_VAR_playbook=/ansible/$PLAYBOOK_NAME -ti cnfdeploytools:latest apply -auto-approve
