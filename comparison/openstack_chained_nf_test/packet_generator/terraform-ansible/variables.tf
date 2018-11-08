bee84a272d6fa6c73b6a35a96b567b98b0bab20d
Robert Starmer <robert@kumul.us>
2018-11-08 06:59:59 +0000

    Fix Issue [#122] addressing order of operations
    Map resources into two domains to allow for chef deployment of OpenStack

--- comparison/openstack_chained_nf_test/packet_generator/terraform-ansible/variables.tf
+++ comparison/openstack_chained_nf_test/packet_generator/terraform-ansible/variables.tf
@@ -2,10 +2,10 @@ provider "packet" {
   auth_token = "${ var.packet_api_key }"
 }
 variable "name" { default = "os-host" }
-variable "packet_node_count" { default = "3" }
+variable "packet_node_count" { default = "4" }
 variable "packet_project_id" {}
 variable "packet_api_key" {}
-variable "packet_facility" { default = "sjc1" }
+variable "packet_facility" { default = "ewr1" }
 variable "packet_billing_cycle" { default = "hourly" }
 
 # VM Image and size
@@ -14,3 +14,4 @@ variable "packet_operating_system" { default = "centos_7" }
 
 #Ansible
 variable "playbook" {}
+variable "vlan" { default= ["1137","1138"] }
--- comparison/openstack_chained_nf_test/packet_generator/terraform.sh
+++ comparison/openstack_chained_nf_test/packet_generator/terraform.sh
@@ -4,5 +4,11 @@ parentdir="$(dirname "$dir")"
 parentdir2="$(dirname "$parentdir")"
 parentdir3="$(dirname "$parentdir2")"
 echo "DIR $parentdir3"
-
 docker run -v $(pwd)/ansible:/ansible -v ~/.ssh/id_rsa:/root/.ssh/id_rsa -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub -v "$dir"/terraform-ansible/:/terraform -e TF_VAR_packet_project_id=${PACKET_PROJECT_ID} -e TF_VAR_packet_api_key=${PACKET_AUTH_TOKEN} -e TF_VAR_playbook=/ansible/main.yml -ti terraform:latest apply -auto-approve
+
+echo "[all]" > ansible/inventory
+cat terraform-ansible/terraform.tfstate | awk -F\" '/0.add/ {print $4}' >> ansible/inventory
+echo "[all:vars]" >> ansible/inventory
+echo "ansible_ssh_extra_args='-o StrictHostKeyChecking=no'" >> ansible/inventory
+
+docker run -v $(pwd)/ansible:/ansible -v ~/.ssh/id_rsa:/root/.ssh/id_rsa -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub -v "$dir"/terraform-ansible/:/terraform -v $(pwd)/ansible/inventory:/etc/ansible/hosts --entrypoint ansible-playbook -ti terraform:latest /ansible/openstack.yml
