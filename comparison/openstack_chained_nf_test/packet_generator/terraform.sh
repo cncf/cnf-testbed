16bd8cf6d47656cd60e11cc7ecd0917549739a20
Robert Starmer <robert@kumul.us>
2018-11-09 07:58:04 +0000

    Fix grub and base VPP install
    Resolve Grub/reboot issue, adding rd.auto rd.auto=1 to the grub
    linux cmdline option.
    
    Add retr0h.etcd from ansible.galax and change the inventory group name to etcd
    
    Add vpp.yml and role
      main file calls vpp/etcd/grub roles to set up base environment
      role configures debian and redhat enviornments for vpp

--- comparison/openstack_chained_nf_test/packet_generator/terraform.sh
+++ comparison/openstack_chained_nf_test/packet_generator/terraform.sh
@@ -6,7 +6,7 @@ parentdir3="$(dirname "$parentdir2")"
 echo "DIR $parentdir3"
 docker run -v $(pwd)/ansible:/ansible -v ~/.ssh/id_rsa:/root/.ssh/id_rsa -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub -v "$dir"/terraform-ansible/:/terraform -e TF_VAR_packet_project_id=${PACKET_PROJECT_ID} -e TF_VAR_packet_api_key=${PACKET_AUTH_TOKEN} -e TF_VAR_playbook=/ansible/main.yml -ti terraform:latest apply -auto-approve
 
-echo "[all]" > ansible/inventory
+echo "[etcd]" > ansible/inventory
 cat terraform-ansible/terraform.tfstate | awk -F\" '/0.add/ {print $4}' >> ansible/inventory
 echo "[all:vars]" >> ansible/inventory
 echo "ansible_ssh_extra_args='-o StrictHostKeyChecking=no'" >> ansible/inventory
