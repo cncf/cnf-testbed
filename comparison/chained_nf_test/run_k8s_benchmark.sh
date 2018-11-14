a8e7ed3e409691d8b446d098940b9b0b7ae0aa2c
denverwilliams <denver@debian.nz>
2018-11-14 01:10:53 -0500

    WIP: layer-2 packet ansible role working for create/delete/(un)assign/(dis)bond [#113]

--- comparison/chained_nf_test/run_k8s_benchmark.sh
+++ comparison/chained_nf_test/run_k8s_benchmark.sh
@@ -2,16 +2,34 @@
 
 ## Deploy k8s
 ## load/set configuration
-#../../tools/deploy_k8s_cluster.sh
+#./deploy_k8s_cluster.sh
+#   - cross-cloud (terraform) => run ansible playbook k8s_cluster.yml 
+#   - include playbooks to setup k8s cluster
+# PACKET
+#   - create vlans (ansible)
+#   - remove ports from bond (ansible)
+#   - assign vlans to ports (ansible)
+# HOST
+#   - removes ports from bond on worker nodes (ansible)
+#   - sets up vpp on worker node (ansible)
 
-## Deploy chained CNFs
-# ./deploy_chained_cnfs.sh
+
+# NOTE: Run the tests for eitehr snake or pipeline to get the total time of each
+
+## Deploy snake CNF topology
+# ./deploy_snake_cnf_topology.sh
+
+## Deploy pipeline CNF topology
+# ./deploy_pipeline_cnf_topology.sh
 
 ## Deploy traffic generator
 pushd ./packet_generator/
 ./deploy_packet_generator.sh
 
 # Run tests
+# ...
+
 popd
 
 ## Collect and summarize results
+# ...
