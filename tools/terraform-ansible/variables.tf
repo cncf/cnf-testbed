d64d873958926034f7448aba679e5273529ef626
Robert Starmer <robert@kumul.us>
2018-11-17 11:20:10 +0000

    Addressed additional VPP config/setup issues for OpenStack
    Add vpp-agent start
    add vpp startup and interface config
    address order of deployment for vlan config

--- tools/terraform-ansible/variables.tf
+++ tools/terraform-ansible/variables.tf
@@ -9,7 +9,7 @@ variable "packet_facility" { default = "sjc1" }
 variable "packet_billing_cycle" { default = "hourly" }
 
 # VM Image and size
-variable "packet_master_device_plan" { default = "c1.xlarge.x86" }
+variable "packet_master_device_plan" { default = "m2.xlarge.x86" }
 variable "packet_operating_system" { default = "ubuntu_16_04" }
 
 #Ansible
