b48d60b0046203c332822c712dd28907dafd6566
Robert Starmer <robert@kumul.us>
2018-11-28 12:20:37 +0000

    Update node config for KVM hugepages

--- comparison/ansible/roles/vpp_openstack/templates/create_flavor.sh
+++ comparison/ansible/roles/vpp_openstack/templates/create_flavor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ~/openrc
-openstack flavor create --id 1 --ram 2048 --disk 10 --property hw:mem_page_size=large small
+openstack flavor create --id 1 --ram 2048 --disk 10 --property hw:mem_page_size=2048 small
