66c729ec4ef457e11d623d9ca9af013f50f2371a
denverwilliams <denver@debian.nz>
2018-12-01 01:16:08 -0500

    Add work around to fetch worker node ip without using terraform #107

--- comparison/kubecon18-chained_nf_test/k8s_worker_override.tf
+++ comparison/kubecon18-chained_nf_test/k8s_worker_override.tf
@@ -4,6 +4,6 @@ resource "packet_device" "workers" {
   hardware_reservation_id = "next-available"
 }
 
-output "server_list" {
-  value = "${ join(",", packet_device.workers.*.hostname) }"
-}
+#output "server_list" {
+#  value = "${ join(",", packet_device.workers.*.hostname) }"
+#}
--- comparison/kubecon18-chained_nf_test/run_k8s_benchmark.sh
+++ comparison/kubecon18-chained_nf_test/run_k8s_benchmark.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+project_root=$(cd ../../ ; pwd -P)
+worker_hostname=$NAME-worker-1.$NAME.packet.local
+worker_ip=$(dig worker.$NAME.packet.local @147.75.69.23 +short)
 ## Deploy k8s
 ## load/set configuration
-#./deploy_k8s_cluster.sh
 ../../tools/deploy_k8s_cluster.sh
+
+docker run \
+       -v "${project_root}/comparison/ansible:/ansible" \
+       -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
+       -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
+       -e PACKET_API_TOKEN=${PACKET_AUTH_TOKEN} \
+       --entrypoint=ansible-playbook \
+       -ti cnfdeploytools:latest -i "$worker_ip," -e 'host_key_checking=False' -e deploy_env=$NAME -e server_list=$NAME-worker-1.$NAME.packet.local /ansible/$PLAYBOOK
+
 #   - cross-cloud (terraform) => run ansible playbook k8s_cluster.yml 
 #   - include playbooks to setup k8s cluster
 # PACKET
