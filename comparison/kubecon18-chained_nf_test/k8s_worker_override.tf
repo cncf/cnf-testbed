b02d7eaeeec0fcfa7579a45a1f67055fc49ff2a5
denverwilliams <denver@debian.nz>
2018-12-04 01:14:03 -0500

    Fetch node ips from kubernetes as a workaround for https://github.com/cncf/cnfs/issues/152 #107

--- comparison/kubecon18-chained_nf_test/k8s_worker_override.tf
+++ comparison/kubecon18-chained_nf_test/k8s_worker_override.tf
@@ -3,7 +3,3 @@
 resource "packet_device" "workers" {
   hardware_reservation_id = "next-available"
 }
-
-#output "server_list" {
-#  value = "${ join(",", packet_device.workers.*.hostname) }"
-#}
--- comparison/kubecon18-chained_nf_test/run_k8s_benchmark.sh
+++ comparison/kubecon18-chained_nf_test/run_k8s_benchmark.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 project_root=$(cd ../../ ; pwd -P)
-worker_hostname=$NAME-worker-1.$NAME.packet.local
-worker_ip=$(dig worker.$NAME.packet.local @147.75.69.23 +short)
+
 ## Deploy k8s
 ## load/set configuration
 ../../tools/deploy_k8s_cluster.sh
+sudo chown $(whoami):$(whoami) $(pwd)/data/kubeconfig
+export KUBECONFIG=$(pwd)/data/kubeconfig
+worker_hostnames=$(kubectl get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="Hostname")].address}')
+worker_ips=$(kubectl get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="InternalIP")].address}')
 
 docker run \
        -v "${project_root}/comparison/ansible:/ansible" \
        -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
        -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
        -e PACKET_API_TOKEN=${PACKET_AUTH_TOKEN} \
+       -e ANSIBLE_HOST_KEY_CHECKING=False \
        --entrypoint=ansible-playbook \
-       -ti cnfdeploytools:latest -i "$worker_ip," -e 'host_key_checking=False' -e deploy_env=$NAME -e server_list=$NAME-worker-1.$NAME.packet.local /ansible/$PLAYBOOK
+       -ti cnfdeploytools:latest -i "$worker_ips," -e deploy_env=${NAME} -e server_list=${worker_hostnames} /ansible/$PLAYBOOK
 
 #   - cross-cloud (terraform) => run ansible playbook k8s_cluster.yml 
 #   - include playbooks to setup k8s cluster
