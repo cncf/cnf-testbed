cec02afb930db1d1dca2f0707997a51dcdd5300f
Robert Starmer <robert@kumul.us>
2018-12-06 06:04:39 +0000

    Update vlan_create script to use provider vs. physnet

--- comparison/ansible/roles/openstack_scripts/templates/create_vlans.sh
+++ comparison/ansible/roles/openstack_scripts/templates/create_vlans.sh
@@ -5,7 +5,7 @@ if [[ $# == '0' ]]; then
   exit 1
 fi
 source ~/openrc
-openstack network create --provider-segment ${1} --provider-network-type vlan --provider-physical-network physnet vlan${1}
+openstack network create --provider-segment ${1} --provider-network-type vlan --provider-physical-network provider vlan${1}
 (( vlan_subnet = $1 - 1000 ))
-openstack subnet create --network vlan${1} --subnet-range 10.${vlan_subnet}.0.0/24 --no-dhcp --gateway none subnet${1}
+openstack subnet create --network vlan${1} --subnet-range 10.${vlan_subnet}.0.0/24  --gateway none subnet${1}
 
