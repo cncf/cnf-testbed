a0fe561b22b49e17cf72fa0c4a1ec15f8d3c70c9
Robert Starmer <robert@kumul.us>
2018-12-06 11:29:44 +0000

    Update OpenStack deployment to support Ubuntu 16.04 #150
    Continued efforts to support Ubuntu as well as CentOS

--- comparison/ansible/roles/openstack_scripts/templates/create_flavor.sh
+++ comparison/ansible/roles/openstack_scripts/templates/create_flavor.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source ~/openrc
-openstack flavor create --id 1 --ram 16384 --disk 10 hp.small
-openstack flavor create --id 2 --ram 32768 --disk 10 hp.large
+if [[ ! $(openstack flavor list | grep hp. | wc -l) -ge 2 ]]; then
+  openstack flavor create --id 1 --ram 16384 --disk 10 hp.small
+  openstack flavor create --id 2 --ram 32768 --disk 10 hp.large
+fi
 # --property hw:numa_nodes=1 --property hw:mem_page_size=2048
--- comparison/ansible/roles/openstack_scripts/templates/create_xenial.sh
+++ comparison/ansible/roles/openstack_scripts/templates/create_xenial.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
-
 source ~/openrc
 if [ ! -f xenial.img ]; then
-curl https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img > xenial.img
+  curl https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img > xenial.img
 fi
-openstack  image create --container-format bare --disk-format qcow2 --min-disk 2 --min-ram 1000 --public --file xenial.img xenial
+if [[ ! $(openstack image list | grep xenial | wc -l) -ge 1 ]]; then
+  openstack  image create --container-format bare --disk-format qcow2 --min-disk 2 --min-ram 1000 --public --file xenial.img xenial
+fi
