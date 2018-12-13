251ed4f56fea44538452b81653861c2517a2653b
Robert Starmer <robert@kumul.us>
2018-12-13 10:20:44 +0000

    Add sysctl hugepage config and additional nova.conf parameters
    Fixes for #101

--- comparison/ansible/roles/openstack_scripts/templates/create_instance.sh
+++ comparison/ansible/roles/openstack_scripts/templates/create_instance.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 source ~/openrc
-network=`openstack network list | awk '/vlan/ {print $4}'`
-openstack  server create test${1} --flavor 1 --image xenial --nic net-id=${network} --property hw:numa_nodes=0 --property hw:mem_page_size=2048
+cat > /tmp/test${1}.cfg <<EOF
+#!/bin/bash
+passwd ubuntu <<EOL
+ubuntu
+ubuntu
+EOL
+EOF
 
+network=`openstack network list | awk '/vlan/ {print $4}'`
+openstack  server create test${1} --flavor 1 --image xenial --nic net-id=${network} --property hw:numa_nodes=0 --property hw:mem_page_size=2048 --config-drive True --user-data /tmp/test${1}.cfg
