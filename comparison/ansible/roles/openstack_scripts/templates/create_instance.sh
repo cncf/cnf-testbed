c3a4aee58088ba63d1b32078b106f4d1bf9e5edb
Robert Starmer <robert@kumul.us>
2018-12-15 01:19:57 +0000

    Add securtiy group and modify instance launch
    Fixes for #101 and #150

--- comparison/ansible/roles/openstack_scripts/templates/create_instance.sh
+++ comparison/ansible/roles/openstack_scripts/templates/create_instance.sh
@@ -9,4 +9,6 @@ EOL
 EOF
 
 network=`openstack network list | awk '/vlan/ {print $4}'`
-openstack  server create test${1} --flavor 1 --image xenial --nic net-id=${network} --property hw:numa_nodes=0 --property hw:mem_page_size=2048 --config-drive True --user-data /tmp/test${1}.cfg
+openstack  server create test${1} --flavor 1 --image xenial --nic net-id=${network} --property hw:mem_page_size=2048 --config-drive True --user-data /tmp/test${1}.cfg
+
+#--property hw:numa_nodes=0
--- comparison/ansible/roles/openstack_scripts/templates/create_security_groups.sh
+++ comparison/ansible/roles/openstack_scripts/templates/create_security_groups.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-admin_default=$(openstack project list | awk '/admin/ {print $2}')
+admin_project=$(openstack project list | awk '/admin/ {print $2}')
+admin_default=$(openstack security group list | grep $admin_project | awk '{print $2}')
 openstack security group rule create --proto icmp ${admin_default}
 openstack security group rule create --proto tcp --dst-port 22 ${admin_default}
 openstack security group rule create --proto tcp --dst-port 80 ${admin_default}
