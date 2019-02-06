519673338dc911e046e949fd3cbb3f336b231167
denverwilliams <denver@debian.nz>
2019-02-05 20:15:27 -0500

    Update vedge build script

--- comparison/kubecon18-chained_nf_test/VNF_openstack/base_image/vedge_vm_build.sh
+++ comparison/kubecon18-chained_nf_test/VNF_openstack/base_image/vedge_vm_build.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
-
 set -o xtrace  # print commands during script execution
 
-if [ "$VEDGE_STATE" == "build" ];
-then 
-    sudo apt-get update -y
-    sudo apt-get install --allow-unauthenticated -y make wget gcc libcurl4-openssl-dev python-pip bridge-utils apt-transport-https ca-certificates -y
-    pip install jsonschema
+sudo apt-get update -y
+sudo apt-get install --allow-unauthenticated -y make gcc libcurl4-openssl-dev python-pip bridge-utils apt-transport-https ca-certificates
+pip install jsonschema
 
-    sudo apt-get -y install linux-headers-$(uname -r)
+sudo apt-get -y install linux-headers-$(uname -r)
 
-    # Install VPP
-    export UBUNTU="xenial"
-    export RELEASE=".stable.1804"
-    sudo rm /etc/apt/sources.list.d/99fd.io.list
-    sudo echo "deb [trusted=yes] https://nexus.fd.io/content/repositories/fd.io$RELEASE.ubuntu.$UBUNTU.main/ ./" | sudo tee -a /etc/apt/sources.list.d/99fd.io.list
-    sudo apt-get update
-    sudo apt-get install -y vpp vpp-dpdk-dkms vpp-lib vpp-dbg vpp-plugins vpp-dev
-    sleep 1
-
-    sudo sed -i 's/^.*\(net.ipv4.ip_forward\).*/\1=1/g' /etc/sysctl.conf
-    sudo sysctl -p /etc/sysctl.conf
-
-    sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="isolcpus=1,2 nohz_full=1,2 rcu_nocbs=1,2"/g' /etc/default/grub
-    sudo update-grub2
-
-    cp /build/igb_uio.ko /lib/modules/$(uname -r)/kernel/drivers/
-    echo 'igb_uio' | sudo tee -a /etc/modules
-    sudo depmod
+# Install VPP
+VPP_VERSION="18.10-release"
+artifacts=()
+vpp=(vpp vpp-dbg vpp-dev vpp-lib vpp-plugins)
+if [ -z "${VPP_VERSION-}" ]; then
+    artifacts+=(${vpp[@]})
 else
-    echo "Build already complete, skip"
+    artifacts+=(${vpp[@]/%/=${VPP_VERSION-}})
 fi
+curl -s https://packagecloud.io/install/repositories/fdio/release/script.deb.sh | bash
+apt-get install -y "${artifacts[@]}"
+sleep 1
+
+sudo sed -i 's/^.*\(net.ipv4.ip_forward\).*/\1=1/g' /etc/sysctl.conf
+sudo sysctl -p /etc/sysctl.conf
+
+sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="isolcpus=1,2 nohz_full=1,2 rcu_nocbs=1,2"/g' /etc/default/grub
+sudo update-grub2
+
+cp /build/igb_uio.ko /lib/modules/$(uname -r)/kernel/drivers/
+echo 'igb_uio' | sudo tee -a /etc/modules
+sudo depmod
+
+sh /build/inject_vagrant_ssh_key.sh
+
+echo "#!/bin/sh -e" | sudo tee /etc/rc.local
+echo "ssh-keygen -A" | sudo tee -a /etc/rc.local
