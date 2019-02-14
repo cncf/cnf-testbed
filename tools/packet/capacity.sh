0623300c2625a2249e16608d95804190586241d1
Robert Starmer <robert@kumul.us>
2019-02-14 01:50:27 +0000

    Remove old Packet API key from script...

--- tools/packet/capacity.sh
+++ tools/packet/capacity.sh
@@ -7,5 +7,5 @@ PLAN=${PLAN:-m2.xlarge.x86}
 
 curl -sX POST --header "Content-Type: application/json" \
              --header "Accept: application/json" \
-             --header "X-Auth-Token: JMTJo5JMcccR4cSGMrmDU5x3aL8hm49v" \
+             --header "X-Auth-Token: ${PACKET_API_TOKEN}" \
              --data "{\"servers\":[{\"facility\": \"${FACILITY}\",\"plan\": \"${PLAN}\",\"quantity\": \"${QUANTITY}\"}]}" https://api.packet.net/capacity | python -mjson.tool
