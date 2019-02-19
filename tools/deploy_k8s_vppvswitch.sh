c4f9b92bdd7ccf5df5a463a1c4ae744739cf1c90
Taylor Carpenter <taylor@vulk.coop>
2019-02-19 17:24:15 -0600

    Update and rename deploy_k8s_vppvswitch_cluster.sh to deploy_k8s_vppvswitch.sh

--- tools/deploy_k8s_vppvswitch.sh
+++ tools/deploy_k8s_vppvswitch.sh
@@ -2,18 +2,29 @@
 
 project_root=$(cd ../ ; pwd -P)
 
-## Deploy k8s
+if [ -z "$1" ] ; then
+  echo "$0 <path_to_kubeconfig>"
+  exit 1
+fi
+
 ## load/set configuration
-SECONDS=0
-$(project_root)/tools/deploy_k8s_cluster.sh
+
+#export KUBECONFIG=$(pwd)/data/kubeconfig
+export KUBECONFIG=$0
+
+if [ ! -f $KUBECONFIG ] ; then
+   echo "Could not find kubeconfig $KUBECONFIG"
+   exit 1
+fi
+
 sudo chown $(whoami):$(whoami) $(pwd)/data/kubeconfig 
-export KUBECONFIG=$(pwd)/data/kubeconfig
+
 worker_hostnames=$(kubectl get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="Hostname")].address}')
 worker_ips=$(kubectl get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="InternalIP")].address}')
 K8S_ELAPSED_TIME=$SECONDS
 
 ## Deploy VPP vSwitch
-SECONDS=0
+#SECONDS=0
 docker run \
        -v "${project_root}/comparison/ansible:/ansible" \
        -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
@@ -21,8 +32,9 @@ docker run \
        -e PACKET_API_TOKEN=${PACKET_AUTH_TOKEN} \
        -e ANSIBLE_HOST_KEY_CHECKING=False \
        --entrypoint=ansible-playbook \
-       -ti cnfdeploytools:latest -i "$worker_ips," -e deploy_env=${NAME} -e server_list=${worker_hostnames} /ansible/$PLAYBOOK
-VSWITCH_ELAPSED_TIME=$SECONDS
+       -ti cnfdeploytools:latest -i "$worker_ips," -e deploy_env=${DEPLOY_ENV} -e server_list=${worker_hostnames} /ansible/$PLAYBOOK
 
-echo "$(($K8S_ELAPSED_TIME / 60)) minutes and $(($K8S_ELAPSED_TIME % 60)) seconds elapsed - K8s Deploy."
-echo "$(($VSWITCH_ELAPSED_TIME / 60)) minutes and $(($VSWITCH_ELAPSED_TIME % 60)) seconds elapsed - vSwitch Deploy."
+#VSWITCH_ELAPSED_TIME=$SECONDS
+
+#echo "$(($K8S_ELAPSED_TIME / 60)) minutes and $(($K8S_ELAPSED_TIME % 60)) seconds elapsed - K8s Deploy."
+#echo "$(($VSWITCH_ELAPSED_TIME / 60)) minutes and $(($VSWITCH_ELAPSED_TIME % 60)) seconds elapsed - vSwitch Deploy."
