44b14959889920ebe6cf0d8c1841874c7a4a0882
Robert Starmer <robert@kumul.us>
2019-03-12 21:51:45 +0000

    Clean up further regressions on Intel Ubuntu 18.04 OpenStack
    Addresses Issue #161
    
    A regression in the Chef code for Rabbit broke all builds, remedied by explicity
    setting the Rabbit version in the chef deploy code
    Address services not being properly restarted after VPP and VPP OpenStack install
    stages.
    Update openstack scripts to enable masquerade resource deployment

--- comparison/ansible/roles/openstack_scripts/templates/create_instance.sh
+++ comparison/ansible/roles/openstack_scripts/templates/create_instance.sh
@@ -9,8 +9,23 @@ EOL
 EOF
 
 FLAVOR=${2:-c0.small}
+if [ ! -f /root/.ssh/id_rsa ] ;then
+ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
+openstack keypair create oskey -f ~/.ssh/id_rsa.pub
+fi
 
+if [ ! "$(openstack server list | grep test${1} | awk '{print $4}')" == "test${1}" ] ; then
 network=`openstack network list | awk '/vlan/ {print $4}' | head -1`
-openstack  server create test${1} --flavor ${FLAVOR} --image xenial --nic net-id=${network} --config-drive True --user-data /tmp/test${1}.cfg
+openstack  server create test${1} --flavor ${FLAVOR} --image xenial --nic net-id=${network} --config-drive True --user-data /tmp/test${1}.cfg --key-name oskey --poll
+fi
 
+if [ "$(openstack network list | awk '/netext/ {print $4}')" == "netext" ] ; then
+float=$(openstack floating ip create netext | awk '/floating_ip_address/ {print $3}')
+port_ip=$(openstack server list | grep test${1} | awk '{print $8}')
+port_id=$(openstack port list | grep ${port_ip} | awk '{print $2}')
 
+openstack floating ip set --port=${port_id} $float}
+fi
+
+echo Log into your server with:
+echo   ubuntu@${float}
