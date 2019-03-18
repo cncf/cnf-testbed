836904e1b36195b489ad6a073cab0f53b6ccbe17
Michael S. Pedersen <michaelx.pedersen@intel.com>
2019-03-18 14:46:09 +0100

    [#205] Update path in script and add README

--- comparison/baseline_nf_performance-packet/host_vpp_container/runner.sh
+++ comparison/baseline_nf_performance-packet/host_vpp_container/runner.sh
@@ -4,6 +4,6 @@ docker run --privileged --tty --detach \
 	--cpuset-cpus 0,2,4,30,32 \
 	--device=/dev/hugepages/:/dev/hugepages/ \
 	--volume "/etc/vpp/sockets/:/etc/vpp/sockets/" \
-	--volume "/root/vSwitch/shared/:/tmp/shared" \
+	--volume "${PWD}/shared/:/tmp/shared" \
 	--volume "/dev/:/dev/" \
 	--name "VPPcontainer" vpp_image
