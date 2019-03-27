66e1adf5e11d5ec71493e63ac5824be79f671ecb
Michael S. Pedersen <michaelx.pedersen@intel.com>
2019-03-27 17:26:09 +0100

    [#205] Small modifications to docker container image
    Done to avoid read-only limitation of Helm configMap, in preparation for K8s CSC chain implementation

--- comparison/baseline_nf_performance-packet/host_vpp_container/runner.sh
+++ comparison/baseline_nf_performance-packet/host_vpp_container/runner.sh
@@ -3,7 +3,7 @@
 docker run --privileged --tty --detach \
 	--cpuset-cpus 0,2,4,30,32 \
 	--device=/dev/hugepages/:/dev/hugepages/ \
+        --volume "${PWD}/shared/:/etc/vpp/" \
 	--volume "/etc/vpp/sockets/:/etc/vpp/sockets/" \
-	--volume "${PWD}/shared/:/tmp/shared" \
 	--volume "/dev/:/dev/" \
 	--name "VPPcontainer" vpp_image
