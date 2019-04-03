c442e876395f64d01b8db8aa84a87f361feda7a6
wolfpack <wolfpack@vulk.co>
2019-04-02 21:45:23 -0400

    don't error out if no servers found

--- comparison/kubecon18-chained_nf_test/VNF_openstack/server_delete.sh
+++ comparison/kubecon18-chained_nf_test/VNF_openstack/server_delete.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
-openstack server list | grep vnf.3c | awk '{print $4}' | xargs -n1 openstack server delete
-echo ""
-echo ""
-echo "Show server list:"
-openstack server list
+server_list=$(openstack server list | grep vnf.3c | awk '{print $4}')
+#openstack server list | grep vnf.3c | awk '{print $4}' | xargs -n1 openstack server delete
+if [ -n "$server_list" ] ; then
+  echo $sever_list | awk '{print $4}' | xargs -n1 openstack server delete
+  echo ""
+  echo ""
+  echo "Show server list:"
+  openstack server list
+else
+  echo "No servers running"
+fi
