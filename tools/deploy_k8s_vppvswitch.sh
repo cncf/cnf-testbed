3e9bd064b9d4821feace1752528f135d68d99fd0
linkous <linkous8@gmail.com>
2019-04-15 15:23:56 -0500

    Tested kubernetes deployments #219

--- tools/deploy_k8s_vppvswitch.sh
+++ tools/deploy_k8s_vppvswitch.sh
@@ -32,9 +32,10 @@ docker run \
        -e PACKET_API_TOKEN=${PACKET_AUTH_TOKEN} \
        -e PROJECT_NAME="${PACKET_PROJECT_NAME}" \
        -e PACKET_FACILITY=${PACKET_FACILITY} \
+       -e K8S_DEPLOY_ENV=${K8S_DEPLOY_ENV} \
        -e ANSIBLE_HOST_KEY_CHECKING=False \
        --entrypoint=ansible-playbook \
-       -ti cnfdeploytools:latest -i "${worker_ips// /,}," -e deploy_env=${K8S_DEPLOY_ENV} -e server_list="${worker_hostnames// /,}" /ansible/$PLAYBOOK
+       -ti cnfdeploytools:latest -i "${worker_ips// /,}," -e server_list="${worker_hostnames// /,}" /ansible/$PLAYBOOK
 
 #VSWITCH_ELAPSED_TIME=$SECONDS
 
--- tools/test_openstack_create.sh
+++ tools/test_openstack_create.sh
@@ -13,6 +13,6 @@ time docker run --rm \
   --entrypoint ansible-playbook -ti cnfdeploytools:latest /ansible/openstack_test_create.yml
 OPENSTACK_VM_CREATE_TIME=$SECONDS
 
-echo "$(($OPENSTACK_VM_CREATE_TIME / 60)) minutes and $(($OPENSTACK_VM_CREATE_TIME % 60)) seconds elapsed - Openstack VM Create."
+echo "$(($OPENSTACK_VM_CREATE_TIME / 60)) minutes and $(($OPENSTACK_VM_CREATE_TIME % 60)) seconds elapsed - Openstack VM create, test, destroy."
 
 
