61640ab4c37af5a24194cb96ea52fd4ec9d5bf07
linkous8 <linkous8@gmail.com>
2019-05-08 12:29:25 -0500

    Delete override.tf

--- tools/terraform-ansible/override.tf
+++
@@ -1,9 +0,0 @@
-# Provision L2 Networking # TODO: delete this file, was only included in check in for debug
-
-resource "packet_device" "master" {
-  hardware_reservation_id = "next-available"
-}
-
-resource "packet_device" "worker" {
-  hardware_reservation_id = "next-available"
-}
