46798e58121e78c1c23334ad480108aaafeccda6
linkous8 <linkous8@gmail.com>
2019-08-30 03:13:21 -0500

    Openstack deploy gets controller IP from tfstate (#256)

--- tools/test_openstack_create.sh
+++ tools/test_openstack_create.sh
@@ -9,7 +9,7 @@ time docker run --rm \
   -v ${parentdir}/comparison/ansible:/ansible \
   -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
   -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
-  -v ${parentdir}/comparison/ansible/inventory:/etc/ansible/hosts \
+  -v ${parentdir}/tools/terraform-ansible/:/terraform \
   --entrypoint ansible-playbook -ti cnfdeploytools:latest /ansible/openstack_test_create.yml
 OPENSTACK_VM_CREATE_TIME=$SECONDS
 
