6f8ad725a38f2b49245a3c528126ee32b06bc577
Michael S. Pedersen <michaelx.pedersen@intel.com>
2019-11-08 15:49:42 +0100

    [#294] Added support for Multus
    Adds ability to create K8s worker with support for Multus and SRIOV

---
+++ comparison/ansible/roles/multus_cni/files/vf_setup.sh
@@ -0,0 +1,67 @@
+#! /bin/bash
+
+function logtime() {
+  date +"%Y-%m-%d %T"
+}
+
+logfile=/var/log/port_setup.log
+
+if [ ! -e "/usr/bin/lspci" ]; then
+  apt-get install pciutils -y
+fi
+
+pf_ids=($(lspci | grep Eth | grep -v Virtual | awk '{print $1}' | head -n 3 | tail -n 2))
+
+if [ ! "${#pf_ids[@]}" == "0" ]; then
+  if [ ! -e "/sys/bus/pci/devices/0000:${pf_ids[0]}/sriov_numvfs" ]; then
+    echo "[$(logtime)] Device virtualization (SRIOV) not enabled - Exiting" >> $logfile
+    exit 0
+  fi
+else
+  echo "[$(logtime)] NIC ports (PCI) not found - Exiting" >> $logfile
+  exit 0
+fi
+
+for pf in ${pf_ids[@]}; do
+  $(echo "4" > /sys/bus/pci/devices/0000:$pf/sriov_numvfs)
+done
+
+vf_ids=($(lspci | grep Eth | grep Virtual | awk '{print $1}'))
+vf_num=${#vf_ids[@]}
+
+if [ "$vf_num" == "0" ]; then
+  echo "[$(logtime)] VFs not found - Exiting" >> $logfile
+  exit 0
+fi
+
+if [ ! "$(($vf_num % 2))" == "0" ]; then
+  echo "[$(logtime)] Odd number of VFs ($vf_num) - Exiting" >> $logfile
+  exit 0
+fi
+
+vf_list=""
+counter=0
+for vf in ${vf_ids[@]}; do
+  if [ "$((($counter) % 4))" -le "1" ]; then
+    vf_list="$vf_list $vf"
+  fi
+  ((counter++))
+done
+
+if [ ! -z "$vf_list" ]; then
+  echo "[$(logtime)] VF list:$vf_list" >> $logfile
+else
+  echo "[$(logtime)] VF list empty - Exiting" >> $logfile
+  exit 0
+fi
+
+if [ ! -e "/opt/cnf_testbed/dpdk-devbind.py" ]; then
+  echo "[$(logtime)] /opt/cnf_testbed/dpdk-devbind.py not found - Exiting" >> $logfile
+  exit 0
+else
+  /usr/bin/python /opt/cnf_testbed/dpdk-devbind.py -b vfio-pci $vf_list
+fi
+
+echo "[$(logtime)] Configuration done" >> $logfile
+
+exit 0
