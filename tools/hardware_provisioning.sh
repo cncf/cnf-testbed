da1dcebeb6cba2e7c53f86ad95db1cb1cf12d00f
Denver Williams <denver@debian.nz>
2019-12-18 16:24:05 +1300

    Add Kubespray support (#309)
    * Add base code for generating kubespray configuration
    
    * Add cli flags for provision & generate_config
    
    * Fix data dir mapping & output correct path for KUBECONFIG
    
    * Update hardware_provisioning script to use the same data folder
    
    * Add code for provisioning a vswitch against a kubespray cluster
    
    * Add vswitch provisioning example

--- tools/hardware_provisioning.sh
+++ tools/hardware_provisioning.sh
@@ -12,6 +12,14 @@ WORKER_PLAN=${WORKER_PLAN:-m2.xlarge.x86}
 SECONDS=0
 HARDWARE_INFRA_DEPLOY=0
 
+STATE_FILE="$(pwd)/data/$DEPLOY_NAME/terraform.tfstate"
+if [ -f "$STATE_FILE" ]; then
+    echo 'a state file for this deployment already exists, exiting'
+    exit 1
+fi
+mkdir -p "$(pwd)/data/$DEPLOY_NAME"
+touch "$STATE_FILE"
+
 if [ "$USE_RESERVED" == "true" ]; then
    RESERVED_CONFIG="-v $(pwd)/hardware-provisioning.reserved:/terraform/override.tf"
 else
@@ -29,17 +37,17 @@ docker run \
         -e TF_VAR_packet_master_device_plan=${MASTER_PLAN} \
         -e TF_VAR_packet_worker_node_count=${WORKER_COUNT} \
         -e TF_VAR_packet_worker_device_plan=${WORKER_PLAN} \
-        -v $(pwd)/$DEPLOY_NAME:/$DEPLOY_NAME \
+        -v $(pwd)/data/$DEPLOY_NAME/terraform.tfstate:/terraform/terraform.tfstate \
         ${RESERVED_CONFIG} \
-        -ti cnfdeploytools:latest apply -auto-approve \
-        -state=/$DEPLOY_NAME/terraform.tfstate
+        -ti cnfdeploytools:latest apply -auto-approve
+        # -state=/$DEPLOY_NAME/terraform.tfstate
          HARDWARE_INFRA_DEPLOY=$SECONDS
 
-touch $(pwd)/"$DEPLOY_NAME"_nodes.env
+touch $(pwd)/data/"$DEPLOY_NAME"/nodes.env
 docker run \
        --rm \
-       -v $(pwd)/$DEPLOY_NAME/terraform.tfstate:/terraform/terraform.tfstate \
-       -v $(pwd)/"$DEPLOY_NAME"_nodes.env:/terraform/nodes.env \
+       -v $(pwd)/data/"$DEPLOY_NAME"/terraform.tfstate:/terraform/terraform.tfstate \
+       -v $(pwd)/data/"$DEPLOY_NAME"/nodes.env:/terraform/nodes.env \
        --entrypoint=/terraform/create_nodes.sh -ti cnfdeploytools:latest
 
 
---
+++ tools/kubernetes_provisioning.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+PROJECT_ROOT=$(cd ../ ; pwd -P)
+DEPLOY_NAME=${DEPLOY_NAME:-cnftestbed}
+if [ "$1" == "generate_config" ]; then
+if ! [ -z ${MASTER_HOSTS+x} ] && ! [ -z ${WORKER_HOSTS+x} ]; then
+   HOSTS_CMD="--master-hosts $MASTER_HOSTS --worker-hosts $WORKER_HOSTS"
+elif ! [ -z ${HOSTS_FILE+x} ]; then
+   HOSTS_TMP="/tmp$HOSTS_FILE"
+   HOSTS_VOLUME="-v $HOSTS_FILE:"
+   HOSTS_CMD="--hosts-file $HOSTS_TMP"
+else
+   echo 'No hosts were found, exiting'
+fi
+fi
+
+# Generate Cluster-Config
+if [ "$1" == "generate_config" ]; then
+    CONFIG_FILE="$(pwd)/data/$DEPLOY_NAME/cluster.yml"
+    if [ -f "$CONFIG_FILE" ]; then
+       echo 'configuration for this deployment already exists, exiting'
+       exit 1
+    fi
+docker run \
+  --rm \
+  -v $(pwd)/data/$DEPLOY_NAME:/k8s-infra/data \
+  $HOSTS_VOLUME$HOSTS_TMP \
+  -ti crosscloudci/k8s-infra:v1.0.0 \
+  /k8s-infra/bin/k8sinfra generate_config ${HOSTS_CMD} --release-type=$RELEASE_TYPE -o /k8s-infra/data/cluster.yml
+fi
+
+#Provision Cluster
+if [ "$1" == "provision" ]; then
+    CLUSTER_DATA="$(pwd)/data/$DEPLOY_NAME/mycluster"
+    if [ -d "$CLUSTER_DATA" ]; then
+        echo 'cluster data for this deployment already exists, exiting'
+        exit 1
+    fi
+docker run \
+  --rm \
+  -v $(pwd)/data/$DEPLOY_NAME:/k8s-infra/data \
+  -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
+  -ti crosscloudci/k8s-infra:v1.0.0 \
+  /k8s-infra/bin/k8sinfra provision --config-file=/k8s-infra/data/cluster.yml
+
+if [ "$?" == "1" ]; then
+   echo 'exit code 1 detected, provisioning failed'
+else
+   echo "Local System KUBECONFIG path: $(pwd)/data/$DEPLOY_NAME/mycluster/artifacts/admin.conf"
+   # echo "To set fix permission errors run: chown $(whoami):$(whoami) $(pwd)/data/$DEPLOY_NAME -R"
+fi
+fi
+
+#Provision vswitch
+if [ "$1" == "vswitch" ]; then
+    if ! [ -z ${WORKER_HOSTS+x} ]; then
+        WORKER_IPS="$WORKER_HOSTS"
+        WORKER_IPS_ARRAY=($(echo $WORKER_HOSTS | tr ',' ' '))
+        WORKER_HOSTNAMES="$(for ((n=1;n<"${#WORKER_IPS_ARRAY[@]}";n++)); do echo -n $DEPLOY_NAME-worker$n,;done;echo -n $DEPLOY_NAME-worker"${#WORKER_IPS_ARRAY[@]}")"
+    elif ! [ -z ${KUBECONFIG+x} ]; then
+        WORKER_IPS_ARRAY=($(docker run -v $KUBECONFIG:/tmp/admin.conf -e KUBECONFIG=/tmp/admin.conf -ti bitnami/kubectl get no -o 'go-template={{range .items}}{{$taints:=""}}{{range .spec.taints}}{{if eq .effect "NoSchedule"}}{{$taints = print $taints .key ","}}{{end}}{{end}}{{if not $taints}}{{range .status.addresses}}{{if (eq .type "InternalIP") }}{{.address}}{{" "}}{{end}}{{end}}{{end}}{{end}}'))
+        WORKER_IPS="$(echo ${WORKER_IPS_ARRAY[@]} | tr ' ', ',')"
+        WORKER_HOSTNAMES="$(for ((n=1;n<"${#WORKER_IPS_ARRAY[@]}";n++)); do echo -n $DEPLOY_NAME-worker$n,;done;echo -n $DEPLOY_NAME-worker"${#WORKER_IPS_ARRAY[@]}")"
+    else
+        echo 'No hosts were found, exiting'
+    fi
+    docker run \
+           -v "${PROJECT_ROOT}/comparison/ansible:/ansible" \
+           -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
+           -e PACKET_API_TOKEN=${PACKET_AUTH_TOKEN} \
+           -e PROJECT_NAME="${PACKET_PROJECT_NAME}" \
+           -e PACKET_FACILITY=${PACKET_FACILITY} \
+           -e K8S_DEPLOY_ENV=${VLAN_SEGMENT} \
+           -e ANSIBLE_HOST_KEY_CHECKING=False \
+           --entrypoint=ansible-playbook \
+           -ti cnfdeploytools:latest -i "${WORKER_IPS}," -e server_list="${WORKER_HOSTNAMES}" /ansible/$PLAYBOOK
+fi
+
+
