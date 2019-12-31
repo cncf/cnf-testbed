a5bd8a1d27142bd93ab567062d70a5fb380e884c
wolfpack <wolfpack@vulk.co>
2019-12-31 13:23:29 -0500

    #vulk/cncf_cnf-testbed/issues/36 tests now hardcoded to facility sjc1

--- tools/packet_api/spec/l2_packet_networking_spec.rb
+++ tools/packet_api/spec/l2_packet_networking_spec.rb
@@ -5,24 +5,25 @@ require "json"
 RSpec.describe 'Packet Test Suite', type: :aruba  do
   context "Packet test" do
     it 'tests bond-interface' do
-      cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --bond-interface eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
+      # cmd = "ruby ../../l2_packet_networking.rb --server cnftestbed-worker1 --bond-interface eth1 --project-name='CNF Testbed' --packet-url='api.packet.net'" 
+      cmd = "ruby ../../l2_packet_networking.rb --server cnftestbed-worker1 --bond-interface eth1 --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1'" 
       run(cmd)
-      sleep(12)
+      sleep(15)
       stop_all_commands
 
       expect(last_command_started.output).to eq("success\n")
     end
 
     it 'tests disbond-interface' do
-      cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --disbond-interface eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
+      cmd = "ruby ../../l2_packet_networking.rb --server cnftestbed-worker1 --disbond-interface eth1 --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1'" 
       run(cmd)
-      sleep(10)
+      sleep(15)
       stop_all_commands
 
       expect(last_command_started.output).to eq("success\n")
     end
     it 'tests create-vlan' do
-      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNCF CNFs' --packet-url='api.packet.net' --facility='ewr1' "
+      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1' "
       run(cmd)
       sleep(10)
       stop_all_commands
@@ -30,28 +31,30 @@ RSpec.describe 'Packet Test Suite', type: :aruba  do
       expect(last_command_started.output.to_i).to be_a_kind_of(Numeric)
     end
     it 'tests assign-vlan and unassign-vlan' do
-      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNCF CNFs' --packet-url='api.packet.net' --facility='ewr1' "
+      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1'"
       run(cmd)
       sleep(10)
       stop_all_commands
 
       expect(last_command_started.output.to_i).to be_a_kind_of(Numeric)
 
-      cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --assign-vlan watsonvlan1 --assign-vlan-port eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
+      # cmd = "ruby ../../l2_packet_networking.rb --server cnftestbed-worker1 --assign-vlan watsonvlan1 --assign-vlan-port eth1 --project-name='CNF Testbed' --packet-url='api.packet.net'" 
+      # run(cmd)
+      # sleep(10)
+      # stop_all_commands
+      #
+      # puts "last command started output: #{last_command_started.output}"
+      # expect(last_command_started.output).to eq("success\n")
+
+      cmd = "ruby ../../l2_packet_networking.rb --server cnftestbed-worker1 --assign-vlan watsonvlan1 --assign-vlan-port eth1 --project-name='CNF Testbed' --packet-url='api.packet.net'   --facility='sjc1'" 
       run(cmd)
       sleep(10)
+
       stop_all_commands
 
-      expect(last_command_started.output).to eq("success\n")
-
-      cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --assign-vlan watsonvlan1 --assign-vlan-port eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
+      cmd = "ruby ../../l2_packet_networking.rb --server cnftestbed-worker1 --unassign-vlan watsonvlan1 --unassign-vlan-port eth1 --project-name='CNF Testbed' --packet-url='api.packet.net'  --facility='sjc1'" 
       run(cmd)
-      sleep(10)
-      stop_all_commands
-
-      cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --unassign-vlan watsonvlan1 --unassign-vlan-port eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
-      run(cmd)
-      sleep(10)
+      sleep(15)
       stop_all_commands
 
       # puts "last command started output: #{last_command_started.output}"
@@ -60,21 +63,21 @@ RSpec.describe 'Packet Test Suite', type: :aruba  do
       expect(last_command_started.output).to eq("success\n")
     end
     it 'tests show-vlan-devices' do
-      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNCF CNFs' --packet-url='api.packet.net' --facility='ewr1' "
+      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1' "
       run(cmd)
       sleep(10)
       stop_all_commands
 
       expect(last_command_started.output.to_i).to be_a_kind_of(Numeric)
 
-      cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --assign-vlan watsonvlan1 --assign-vlan-port eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
+      cmd = "ruby ../../l2_packet_networking.rb --server cnftestbed-worker1 --assign-vlan watsonvlan1 --assign-vlan-port eth1 --project-name='CNF Testbed' --packet-url='api.packet.net'" 
       run(cmd)
       sleep(10)
       stop_all_commands
 
       expect(last_command_started.output.to_i).to be_a_kind_of(Numeric)
 
-      cmd = "ruby ../../l2_packet_networking.rb --show-vlan-devices watsonvlan1 --project-name='CNCF CNFs' --packet-url='api.packet.net' --facility='ewr1'"
+      cmd = "ruby ../../l2_packet_networking.rb --show-vlan-devices watsonvlan1 --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1'"
       run(cmd)
       sleep(10)
       stop_all_commands
@@ -83,21 +86,21 @@ RSpec.describe 'Packet Test Suite', type: :aruba  do
       expect(parsed_response["id"]).to be_a(String) 
     end
     it 'tests show-project_vlans' do
-      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNCF CNFs' --packet-url='api.packet.net' --facility='ewr1' "
+      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1' "
       run(cmd)
       sleep(10)
       stop_all_commands
 
       expect(last_command_started.output.to_i).to be_a_kind_of(Numeric)
 
-      cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --assign-vlan watsonvlan1 --assign-vlan-port eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
+      cmd = "ruby ../../l2_packet_networking.rb --server cnftestbed-worker1 --assign-vlan watsonvlan1 --assign-vlan-port eth1 --project-name='CNF Testbed' --packet-url='api.packet.net'" 
       run(cmd)
       sleep(10)
       stop_all_commands
 
       expect(last_command_started.output.to_i).to be_a_kind_of(Numeric)
 
-      cmd = "ruby ../../l2_packet_networking.rb --show-project-vlans --project-name='CNCF CNFs' --packet-url='api.packet.net' --facility='ewr1'"
+      cmd = "ruby ../../l2_packet_networking.rb --show-project-vlans --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1'"
 
       run(cmd)
       sleep(10)
@@ -108,21 +111,21 @@ RSpec.describe 'Packet Test Suite', type: :aruba  do
     end
 
     it 'tests show-server-ports' do
-      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNCF CNFs' --packet-url='api.packet.net' --facility='ewr1' "
+      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1' "
       run(cmd)
       sleep(10)
       stop_all_commands
 
       expect(last_command_started.output.to_i).to be_a_kind_of(Numeric)
 
-      cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --assign-vlan watsonvlan1 --assign-vlan-port eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
+      cmd = "ruby ../../l2_packet_networking.rb --server cnftestbed-worker1 --assign-vlan watsonvlan1 --assign-vlan-port eth1 --project-name='CNF Testbed' --packet-url='api.packet.net'" 
       run(cmd)
       sleep(10)
       stop_all_commands
 
       expect(last_command_started.output.to_i).to be_a_kind_of(Numeric)
 
-      cmd = "ruby ../../l2_packet_networking.rb --show-server-ports layer2test-01 --project-name='CNCF CNFs' --packet-url='api.packet.net' --facility='ewr1'"
+      cmd = "ruby ../../l2_packet_networking.rb --show-server-ports cnftestbed-worker1 --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1'"
 
       run(cmd)
       sleep(10)
@@ -133,37 +136,37 @@ RSpec.describe 'Packet Test Suite', type: :aruba  do
     end
 
     it 'tests assign-vlan-id' do
-      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNCF CNFs' --packet-url='api.packet.net' --facility='ewr1'"
+      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1'"
       run(cmd)
       sleep(12)
       vid = last_command_started.output.strip
 
-      cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --assign-vlan-id #{vid} --assign-vlan-port eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
+      cmd = "ruby ../../l2_packet_networking.rb --server cnftestbed-worker1 --assign-vlan-id #{vid} --assign-vlan-port eth1 --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1'" 
       run(cmd)
-      sleep(12)
+      sleep(15)
+      stop_all_commands
+      # expect(last_command_started.output).to eq("success\n")
+
+      cmd = "ruby ../../l2_packet_networking.rb --server cnftestbed-worker1 --unassign-vlan-id #{vid} --unassign-vlan-port eth1 --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1'" 
+      run(cmd)
+      sleep(15)
       stop_all_commands
       expect(last_command_started.output).to eq("success\n")
 
-      cmd = "ruby ../../l2_packet_networking.rb --server layer2test-01 --unassign-vlan-id #{vid} --unassign-vlan-port eth1 --project-name='CNCF CNFs' --packet-url='api.packet.net'" 
+      cmd = "ruby ../../l2_packet_networking.rb --delete-vlan watsonvlan1 --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1'  "
       run(cmd)
-      sleep(12)
-      stop_all_commands
-      expect(last_command_started.output).to eq("success\n")
-
-      cmd = "ruby ../../l2_packet_networking.rb --delete-vlan watsonvlan1 --project-name='CNCF CNFs' --packet-url='api.packet.net' --facility='ewr1'  "
-      run(cmd)
-      sleep(12)
+      sleep(15)
       stop_all_commands
       expect(last_command_started.output).to eq("success\n")
     end
 
     it 'tests delete-vlan' do
-      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNCF CNFs' --packet-url='api.packet.net' --facility='ewr1' "
+      cmd = "ruby ../../l2_packet_networking.rb --create-vlan watsonvlan1 --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1' "
       run(cmd)
       sleep(10)
       stop_all_commands
 
-      cmd = "ruby ../../l2_packet_networking.rb --delete-vlan watsonvlan1 --project-name='CNCF CNFs' --packet-url='api.packet.net' --facility='ewr1'  "
+      cmd = "ruby ../../l2_packet_networking.rb --delete-vlan watsonvlan1 --project-name='CNF Testbed' --packet-url='api.packet.net' --facility='sjc1'  "
       run(cmd)
       sleep(10)
       stop_all_commands
