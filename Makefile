9a31fe8136e99c4129d03bcbccf9991379011e22
denverwilliams <denver@debian.nz>
2020-01-01 18:23:01 -0500

    Update hw-provisioning.sh to support provisioning hardware for the packet_gen vulk/cncf_cnf-testbed#36

--- Makefile
+++ Makefile
@@ -13,12 +13,31 @@
 PROJECT_ROOT := $$(pwd -P)
 DEPLOY_NAME := cnftestbed
 KUBECONFIG := $(PROJECT_ROOT)/data/$(DEPLOY_NAME)/mycluster/artifacts/admin.conf
-MASTER_PLAN = m2.xlarge.x86
-WORKER_PLAN = n2.xlarge.x86
 PACKET_PROJECT_NAME = 'Cross-Cloud CI'
 PLAYBOOK := k8s_worker_vswitch_quad_intel.yml
+
+.PHONY: hw_k8s
+hw_k8s : STATE_FILE=$(PROJECT_ROOT)/data/$(DEPLOY_NAME)/kubernetes.tfstate
+hw_k8s : NODE_FILE=$(PROJECT_ROOT)/data/$(DEPLOY_NAME)/kubernetes.env
+hw_k8s : NODE_GROUP_ONE_NAME=$(DEPLOY_NAME)-master
+hw_k8s : NODE_GROUP_TWO_NAME=$(DEPLOY_NAME)-worker
+hw_k8s : NODE_GROUP_ONE_COUNT = 1
+hw_k8s : NODE_GROUP_TWO_COUNT = 2
+hw_k8s : NODE_GROUP_ONE_PLAN = m2.xlarge.x86
+hw_k8s : NODE_GROUP_TWO_PLAN = n2.xlarge.x86
+hw_k8s: hw
+
+.PHONY: hw_pktgen
+hw_pktgen : STATE_FILE=$(PROJECT_ROOT)/data/$(DEPLOY_NAME)/packet_gen.tfstate
+hw_pktgen : NODE_FILE=$(PROJECT_ROOT)/data/$(DEPLOY_NAME)/packet_gen.env
+hw_pktgen : NODE_GROUP_ONE_NAME=$(DEPLOY_NAME)-pktgen
+hw_pktgen : NODE_GROUP_ONE_COUNT = 1
+hw_pktgen : NODE_GROUP_TWO_COUNT = 0
+hw_pktgen : NODE_GROUP_ONE_PLAN = n2.xlarge.x86
+hw_pktgen: hw
+
 hw:
-	MASTER_PLAN=$(MASTER_PLAN) WORKER_PLAN=$(WORKER_PLAN) tools/hardware_provisioning.sh
+	NODE_GROUP_ONE_NAME=$(NODE_GROUP_ONE_NAME) NODE_GROUP_TWO_NAME=$(NODE_GROUP_TWO_NAME) NODE_GROUP_ONE_COUNT=$(NODE_GROUP_ONE_COUNT) NODE_GROUP_TWO_COUNT=$(NODE_GROUP_TWO_COUNT) NODE_GROUP_ONE_PLAN=$(NODE_GROUP_ONE_PLAN) NODE_GROUP_TWO_PLAN=$(NODE_GROUP_TWO_PLAN) STATE_FILE=$(STATE_FILE) NODE_FILE=$(NODE_FILE) tools/hardware_provisioning.sh
 
 .PHONY: k8s
 k8s: config provision
--- tools/hardware_provisioning.sh
+++ tools/hardware_provisioning.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-USE_RESERVED=${USE_RESERVED:-false}
 DEPLOY_NAME=${DEPLOY_NAME:-cnftestbed}
-PACKET_FACILITY=${PACKET_FACILITY:-sjc1}
-PACKET_OS=${PACKET_OS:-ubuntu_16_04}
-MASTER_COUNT=${MASTER_COUNT:-1}
-MASTER_PLAN=${MASTER_PLAN:-m2.xlarge.x86}
-WORKER_COUNT=${WORKER_COUNT:-1}
-WORKER_PLAN=${WORKER_PLAN:-n2.xlarge.x86}
+STATE_FILE=${STATE_FILE:-$(pwd)/data/$DEPLOY_NAME/terraform.tfstate}
+NODE_FILE=${NODE_FILE:-$(pwd)/data/$DEPLOY_NAME/nodes.env}
+USE_RESERVED=${USE_RESERVED:-false}
+
+NODE_GROUP_ONE_NAME=${NODE_GROUP_ONE_NAME:-$DEPLOY_NAME-master}
+NODE_GROUP_TWO_NAME=${NODE_GROUP_ONE_NAME:-$DEPLOY_NAME-worker}
+NODE_GROUP_ONE_COUNT=${NODE_GROUP_ONE_COUNT:-1}
+NODE_GROUP_TWO_COUNT=${NODE_GROUP_TWO_COUNT:-1}
+FACILITY=${FACILITY:-sjc1}
+NODE_GROUP_ONE_DEVICE_PLAN=${NODE_GROUP_ONE_DEVICE_PLAN:-m2.xlarge.x86}
+NODE_GROUP_TWO_DEVICE_PLAN=${NODE_GROUP_TWO_DEVICE_PLAN:-n2.xlarge.x86}
+OPERATING_SYSTEM=${OPERATING_SYSTEM:-ubuntu_16_04}
+
 
 SECONDS=0
 HARDWARE_INFRA_DEPLOY=0
 
-STATE_FILE="$(pwd)/data/$DEPLOY_NAME/terraform.tfstate"
+mkdir -p "$(pwd)/data/$DEPLOY_NAME"
+
+if [ -f "$NODE_FILE" ]; then
+    echo 'a node file for this deployment already exists, exiting'
+    exit 1
+fi
+touch "$NODE_FILE"
+
 if [ -f "$STATE_FILE" ]; then
     echo 'a state file for this deployment already exists, exiting'
     exit 1
 fi
-mkdir -p "$(pwd)/data/$DEPLOY_NAME"
 touch "$STATE_FILE"
 
 if [ "$USE_RESERVED" == "true" ]; then
@@ -28,26 +40,28 @@ fi
 
 docker run \
         --rm \
-        -e TF_VAR_name=${DEPLOY_NAME} \
-        -e TF_VAR_packet_api_key=${PACKET_AUTH_TOKEN} \
-        -e TF_VAR_packet_project_id=${PACKET_PROJECT_ID} \
-        -e TF_VAR_packet_facility=${PACKET_FACILITY} \
-        -e TF_VAR_packet_operating_system=${PACKET_OS} \
-        -e TF_VAR_packet_master_node_count=${MASTER_COUNT} \
-        -e TF_VAR_packet_master_device_plan=${MASTER_PLAN} \
-        -e TF_VAR_packet_worker_node_count=${WORKER_COUNT} \
-        -e TF_VAR_packet_worker_device_plan=${WORKER_PLAN} \
-        -v $(pwd)/data/$DEPLOY_NAME/terraform.tfstate:/terraform/terraform.tfstate \
+        -e TF_VAR_packet_auth_token=${PACKET_AUTH_TOKEN} \
+        -e TF_VAR_node_group_one_project_id=${PROJECT_ID} \
+        -e TF_VAR_node_group_two_project_id=${PROJECT_ID} \
+        -e TF_VAR_node_group_one_name=${NODE_GROUP_ONE_NAME} \
+        -e TF_VAR_node_group_two_name=${NODE_GROUP_TWO_NAME} \
+        -e TF_VAR_node_group_one_count=${NODE_GROUP_ONE_COUNT} \
+        -e TF_VAR_node_group_two_count=${NODE_GROUP_TWO_COUNT} \
+        -e TF_VAR_node_group_one_facility=${FACILITY} \
+        -e TF_VAR_node_group_two_facility=${FACILITY} \
+        -e TF_VAR_node_group_one_device_plan=${NODE_GROUP_ONE_DEVICE_PLAN} \
+        -e TF_VAR_node_group_two_device_plan=${NODE_GROUP_TWO_DEVICE_PLAN} \
+        -e TF_VAR_node_group_one_operating_system=${OPERATING_SYSTEM} \
+        -e TF_VAR_node_group_two_operating_system=${OPERATING_SYSTEM} \
+        -v $STATE_FILE:/terraform/terraform.tfstate \
         ${RESERVED_CONFIG} \
         -ti cnfdeploytools:latest apply -auto-approve
-        # -state=/$DEPLOY_NAME/terraform.tfstate
          HARDWARE_INFRA_DEPLOY=$SECONDS
 
-touch $(pwd)/data/"$DEPLOY_NAME"/nodes.env
 docker run \
        --rm \
-       -v $(pwd)/data/"$DEPLOY_NAME"/terraform.tfstate:/terraform/terraform.tfstate \
-       -v $(pwd)/data/"$DEPLOY_NAME"/nodes.env:/terraform/nodes.env \
+       -v $STATE_FILE:/terraform/terraform.tfstate \
+       -v $NODE_FILE:/terraform/nodes.env \
        --entrypoint=/terraform/create_nodes.sh -ti cnfdeploytools:latest
 
 
