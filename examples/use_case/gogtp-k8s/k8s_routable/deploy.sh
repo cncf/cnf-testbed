75a5f86ea195de8b3ae6e24de2a7f84d668ae0da
denverwilliams <denver@debian.nz>
2020-01-27 16:02:04 -0500

    Add missing pgw address & use export so envsubst will parse vars correctly

--- examples/use_case/gogtp-k8s/k8s_routable/deploy.sh
+++ examples/use_case/gogtp-k8s/k8s_routable/deploy.sh
@@ -3,11 +3,11 @@
 kubectl create -f pgw.yml
 kubectl create -f sgw.yml
 sleep 5
-PGW_IP=$(kubectl get pods gogtp-pgw -o jsonpath='{.status.podIP}')
-SGW_IP=$(kubectl get pods gogtp-sgw -o jsonpath='{.status.podIP}')
+export PGW_IP=$(kubectl get pods gogtp-pgw -o jsonpath='{.status.podIP}')
+export SGW_IP=$(kubectl get pods gogtp-sgw -o jsonpath='{.status.podIP}')
 kubectl create -f mme-configmap.yml
 envsubst < mme.yml | kubectl create -f -
 sleep 5
-MME_IP=$(kubectl get pods gogtp-mme -o jsonpath='{.status.podIP}')
+export MME_IP=$(kubectl get pods gogtp-mme -o jsonpath='{.status.podIP}')
 kubectl create -f enb-configmap.yml
 envsubst < enb.yml | kubectl create -f -
