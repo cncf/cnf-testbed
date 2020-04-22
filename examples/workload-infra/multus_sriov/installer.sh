46ab22a2c86a9a8a6042f3a5279343b8f26d80f2
Michael S. Pedersen <michaelx.pedersen@intel.com>
2020-04-22 10:54:23 +0200

    Update SRIOV DP/CNI to new K8s
    Updated SRIOV CNI binary and Device Plugin image. Also removed Multus installation (included in Kubespray K8s deployment)

--- examples/workload-infra/multus_sriov/installer.sh
+++ examples/workload-infra/multus_sriov/installer.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 if [ $# -eq 0 ]; then
-  kubectl apply -f multus-daemonset.yaml -f configmap.yaml -f sriovdp-daemonset.yaml -f sriov-crd.yaml
+  kubectl apply -f configmap.yaml -f sriovdp-daemonset-v3.2.yaml -f sriov-crd.yaml
 elif [ "$1" == "del" ]; then
-  kubectl delete -f sriov-crd.yaml -f sriovdp-daemonset.yaml -f configmap.yaml -f multus-daemonset.yaml
+  kubectl delete -f sriov-crd.yaml -f sriovdp-daemonset-v3.2.yaml -f configmap.yaml
 else
  echo "Usage: $0 [del]" 
 fi
